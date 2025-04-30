c0fd213229f9fcdecb270015cd182c517afbc1a2
vepaan <141347566+vepaan@users.noreply.github.com>
2025-04-30 17:17:14 -0500

    adding agentic changes to snake_game

--- backend/snake_game.py
+++ backend/snake_game.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
+import numpy as np
 
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
@@ -23,7 +24,7 @@ BLACK = (0,0,0)
 BLOCK_SIZE = 20
 SPEED = 20
 
-class SnakeGame:
+class SnakeGameAI:
 
     def __init__(self, w=640, h=480):
         self.w = w
@@ -31,7 +32,10 @@ class SnakeGame:
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption("SnakeGame")
         self.clock = pygame.time.Clock()
+        self.reset()
 
+    
+    def reset(self):
         self.direction = Direction.RIGHT
 
         self.head = Point(self.w/2, self.h/2)
@@ -42,6 +46,7 @@ class SnakeGame:
         self.score = 0
         self.food = None
         self._place_food()
+        self.frame_iteration = 0
 
 
     def _place_food(self):
@@ -52,59 +57,75 @@ class SnakeGame:
             self._place_food()
 
 
-    def play_step(self):
+    def play_step(self, action):
+        self.frame_iteration += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.direction = Direction.LEFT
-                elif event.key == pygame.K_RIGHT:
-                    self.direction = Direction.RIGHT
-                elif event.key == pygame.K_UP:
-                    self.direction = Direction.UP
-                elif event.key == pygame.K_DOWN:
-                    self.direction = Direction.DOWN
 
-        self._move(self.direction)
+        self._move(action)
         self.snake.insert(0, self.head)
 
+        reward = 0
         game_over = False
-        if self._is_collision():
+
+        #if our agent does not do anything for 100 frames, and doesn't improve, we quit game
+        #longer snake has more time to play around
+        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
-            return game_over, self.score
+            reward = -10
+            return reward, game_over, self.score
         
         if self.head == self.food:
             self.score += 1
+            reward = 10
             self._place_food()
         else:
             self.snake.pop()
 
         self._update_ui()
         self.clock.tick(SPEED)
-        return game_over, self.score
+        return reward, game_over, self.score
 
 
-    def _move(self, direction):
+    def _move(self, action):
+        #action should be in form [straight, right, left]
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clock_wise.index(self.direction)
+        
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clock_wise[idx] #no change
+        elif np.array_equal(action, [0, 1, 0]):
+            next_idx = (idx+1) % 4
+            new_dir = clock_wise[next_idx] #right turn
+        elif np.array_equal(action, [0, 0, 1]):
+            next_idx = (idx-1)%4
+            new_dir = clock_wise[next_idx] #left turn
+
+        self.direction = new_dir
+
         x = self.head.x
         y = self.head.y
-        if direction == Direction.RIGHT:
+        if self.direction == Direction.RIGHT:
             x += BLOCK_SIZE
-        elif direction == Direction.LEFT:
+        elif self.direction == Direction.LEFT:
             x -= BLOCK_SIZE
-        elif direction == Direction.DOWN:
+        elif self.direction == Direction.DOWN:
             y += BLOCK_SIZE
-        elif direction == Direction.UP:
+        elif self.direction == Direction.UP:
             y -= BLOCK_SIZE
             
         self.head = Point(x, y)
 
     
-    def _is_collision(self):
-        if self.head.x > self.w - BLOCK_SIZE or self.head.x < 0 or self.head.y > self.h - BLOCK_SIZE or self.head.y < 0:
+    def _is_collision(self, pt=None):
+        if pt is None:
+            pt = self.head
+
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
-        if self.head in self.snake[1:]:
+        if pt in self.snake[1:]:
             return True
         return False
 
@@ -121,16 +142,4 @@ class SnakeGame:
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
-
-
-if __name__ == "__main__":
-    game = SnakeGame()
-    while True:
-        game_over, score = game.play_step()
-        if game_over:
-            break
-
-    print("Final score: ", score)
-    pygame.quit()
-
 
