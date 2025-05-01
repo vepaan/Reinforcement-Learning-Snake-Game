9d7f13409069250e1368a004e8133935fd408ce8
vepaan <141347566+vepaan@users.noreply.github.com>
2025-04-30 20:37:19 -0500

    adding model as a linear qnet and adding boilerplate for trainer

--- backend/agent.py
+++ backend/agent.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -13,10 +14,10 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 #param to control stochasticity
-        self.gamma = 0 #discount rate
+        self.gamma = 0.9 #discount rate < 1
         self.memory = deque(maxlen=MAX_MEMORY)
-        self.model = None
-        self.trainer = None
+        self.model = Linear_QNet(11, 256, 3) #11 states, 3 moves
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
 
     def get_state(self, game):
@@ -95,7 +96,7 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0) #will call forward function
             move = torch.argmax(prediction).item()
             final_move[move] = 1
 
@@ -133,7 +134,7 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record', record)
 
